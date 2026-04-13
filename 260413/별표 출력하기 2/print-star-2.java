b6ef10f5ab2a2d12adb5cf2394cda7da7579df5a
hansol <ggksthf29@gmail.com>
2026-04-13 21:59:13 +0900

    Title: 별표 출력하기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260413/별표 출력하기 2/print-star-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        for(int i = N; i >= 1; i--){
+            for(int j = 0; j < i; j++){
+                System.out.printf("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
