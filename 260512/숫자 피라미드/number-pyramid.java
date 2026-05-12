e0aa0ec2281e0d67db03f336411b84c6c03e3b89
hansol <180262795+hansolChoi29@users.noreply.github.com>
2026-05-12 13:21:56 +0900

    [Passed] 숫자 피라미드 (133ms, 10MB)

---
+++ 260512/숫자 피라미드/number-pyramid.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        // N개 만큼 그럼 * N
+        Scanner sc = new Scanner(System.in);
+        int N  = sc.nextInt();
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
