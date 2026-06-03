aaa083c91b00bf4b2518015d3080ac0241ecc459
hansol <180262795+hansolChoi29@users.noreply.github.com>
2026-06-03 09:40:13 +0900

    [Passed] 5의 배수의 개수는? (133ms, 10MB)

---
+++ 260603/5의 배수의 개수는?/the-number-of-multiples-of-5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int array[][] = new int[4][4];
+        int count = 0;
+
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                array[i][j] = sc.nextInt();
+
+                if(array[i][j] % 5 == 0){
+                    count++;
+                }
+            }
+        }        
+        System.out.println(count);
+    }
+}
