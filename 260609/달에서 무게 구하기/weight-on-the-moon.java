2c2a9003a95ba2d389699526004b37c2293eb528
hansol <180262795+hansolChoi29@users.noreply.github.com>
2026-06-09 08:59:36 +0900

    [Passed] 달에서 무게 구하기 (103ms, 9MB)

---
+++ 260609/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int 추무게 = 13;
+        double 달중력비율 = 0.165;
+
+        System.out.printf("%d * %.6f = %.6f", 추무게, 달중력비율, 추무게*달중력비율);
+    }
+}
