e3429b1f2ec632201e6f11feb7b56c32fa9f925c
hansol <180262795+hansolChoi29@users.noreply.github.com>
2026-06-09 09:05:26 +0900

    [Passed] 길이 단위 변환하기 (109ms, 9MB)

---
+++ 260609/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        // 1피트 : 30.48
+        // 1마일 : 160934
+        double a = 9.2;
+        double b = 1.3;
+        System.out.printf("%.1fft = %.1fcm \n", a, (a * 30.48));
+        System.out.printf("%.1fmi = %.1fcm", b, (b * 160934));
+    }
+}
