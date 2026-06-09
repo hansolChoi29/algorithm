f0755d044aaa3165275c2f9b25112c2719a7ea47
hansol <180262795+hansolChoi29@users.noreply.github.com>
2026-06-09 09:17:26 +0900

    [Passed] 변수 값 교체하기 3 (98ms, 9MB)

---
+++ 260609/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 3;
+        int b = 5;
+        int temp = b;
+        b = a;
+        a = temp;
+        System.out.printf("%d\n%d", a, b);
+    }
+}
