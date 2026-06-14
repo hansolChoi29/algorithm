c8ecb7858df5e56602867bb4a923517daabd2c88
hansol <180262795+hansolChoi29@users.noreply.github.com>
2026-06-14 14:08:39 +0900

    [Passed] 날짜 변경하여 출력 (122ms, 10MB)

---
+++ 260614/날짜 변경하여 출력/print-date-with-different-format.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+       String str = sc.next();
+       String[] newStr = str.split("\\.");
+
+        System.out.printf("%s-%s-%s", newStr[1], newStr[2], newStr[0]);
+    }
+}
