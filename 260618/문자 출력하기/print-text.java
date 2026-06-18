bc31799d55bef02484f3bff2c847af3869027bdc
hansol <180262795+hansolChoi29@users.noreply.github.com>
2026-06-18 10:21:22 +0900

    [Passed] 문자 출력하기 (123ms, 10MB)

---
+++ 260618/문자 출력하기/print-text.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        char n = sc.next().charAt(0);
+        for(int i = 0; i <= 7; i++){
+            System.out.print(n);
+        }
+    }
+}
