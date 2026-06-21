f63b1fdd9409b9f98909fe9131e808cbf847ea1a
hansol <180262795+hansolChoi29@users.noreply.github.com>
2026-06-21 14:45:13 +0900

    [Passed] 홀수만 더하기 (129ms, 10MB)

---
+++ 260621/홀수만 더하기/add-only-odd-numbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int N = sc.nextInt();
+
+        for(int i = 0; i < N; i++){
+            int num = sc.nextInt();
+
+            if(num % 2 != 0 && num % 3 == 0){
+                sum += num;    
+            }
+        }
+        System.out.println(sum);
+    }
+}
